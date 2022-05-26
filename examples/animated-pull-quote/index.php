5e544deb9304c29ec48d4d3d048f0d5d712011aa
csilverman <chris@csilverman.com>
2022-05-26 16:01:53 -0400

    More work on the animated pullquote; set up CSS var overrides for rightshift animation

--- examples/animated-pull-quote/index.php
+++ examples/animated-pull-quote/index.php
@@ -23,7 +23,8 @@ $page_title = "Animated pull quote";
 
 <?php echo sec_wordByWord(
   ['url' => 'https://images.unsplash.com/photo-1653559178437-94889263fb39?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2942',
-  'alt' => 'asdfasdf']
+  'alt' => 'asdfasdf'],
+  'animate-when-content-appears'
 ); ?>
 
 <p class="word-by-word">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
