e71bef3c134efe3f1e1b1751f05d3ec81df40668
csilverman <chris@csilverman.com>
2022-06-09 11:37:09 -0400

    Homepage work

--- mockups/admissions-home/index.php
+++ mockups/admissions-home/index.php
@@ -5,20 +5,19 @@ include( $project_paths['main_project_root'] . '/functions.php');
 
 ?>
 
-<!-- %%% -->
-
 <?php
 
 /*  PAGE INFO ============ */
 
 $page_title = "Vassar Admission";
-$page_docs = "https://docs.google.com/document/d/10cN3CBi-jaEj5DT_8HQSA3VcsiM2mQ1F2lWxE1yMeSI/edit#heading=h.laz1za8kgg3h";
 
 /*  ---------------------- */
 
 ?>
 
 <?php echo site_header(); ?>
+
+<!--<div class="buffer"></div>-->
 
 <?php echo sec_siteHeader_video(); ?>
   <a href="" class="btn btn-lg btn-primary">Apply</a>
@@ -41,135 +40,283 @@ $page_docs = "https://docs.google.com/document/d/10cN3CBi-jaEj5DT_8HQSA3VcsiM2mQ
 
 
 
-
-<?php echo sec_wideContent(
-  'Your college journey begins here.',
-  '<p class="section-intro-text">Our Admission team is here to help you with:</p>',
-  'show-image-then-content',
+<?php echo sec_fixedCenteredTitle(
+  '<b class="slide-from-left">Your college journey</b>
+   <b class="slide-from-right nice-big-serif">begins here.</b>',
+   '<p class="section-intro-text">Our Admission team is here to help you with:</p>',
+  'sec-fixedCenteredTitle',
   'mw-4',
-    ['bg-image-url' => 'https://images.unsplash.com/photo-1652703747774-558a10faacc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80',
+    ['bg-image-url' => 'journey/0100-21-10-kr-fw-general-vassar-0057.jpg',
     'bg-image-alt' => 'alt text',
-    'title_classes' => 'animate-when-content-appears animation-zoom-in'
-  ]
+    'css' => '
+      --title-container-bg-beforeContent: rgba(0,0,0,0.2);
+      --section-title-faded-opacity: 0.1;
+      --section-bg-image-opacity: 0.1;
+      --section-title-faded-color: #000;
+      '
+    ]
+
 ); ?>
 
-<div class="grid cols-3 gap-0 animation-group animate-when-parent-content-appears animation-zoom-in">
+<div class="grid cols-3">
+  <?php echo item_cardWithText(
+      'Applying to Vassar',
+      '',
+      ['url' => 'apply/untitled-3.jpg', 'alt' => 'asdfasdfds' ],
+      'animation-item'
+    ); ?>
 
-<?php echo item_col('animation-item'); ?>
-  <?php echo item_imageCard(
-    get_icon('play').'Card title',
-    null,
-    'apple.com',
-    ['url' => 'https://images.unsplash.com/photo-1652703747774-558a10faacc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80', 'alt' => 'asdfasdfds' ],
-    'text-at-bottom',
-    ['hlevel' => 4]
+    <p>What’s required / How to apply / Important Deadlines / Tuition</p>
+    <p><strong>Learn more</strong> →</p>
+
+  <?php echo end_item_cardWithText(); ?>
+
+
+  <?php echo item_cardWithText(
+      'Financial Aid Information',
+      '',
+      ['url' => 'apply/untitled-2.jpg', 'alt' => 'asdfasdfds' ],
+      'animation-item'
+    ); ?>
+
+    <p>Early and regular decision / Transferring to Vassar</p>
+    <p><strong>Learn more</strong> →</p>
+
+  <?php echo end_item_cardWithText(); ?>
+
+
+  <?php echo item_cardWithText(
+      'Request Information',
+      '',
+      ['url' => 'apply/untitled-1.jpg', 'alt' => 'asdfasdfds' ],
+      'animation-item'
+    ); ?>
+
+    <p>Have questions? Need Information? Get Vassar’s newsletter and other information about Vassar.</p>
+    <p><strong>Learn more</strong> →</p>
+
+  <?php echo end_item_cardWithText(); ?>
+
+
+
+</div><!-- end layout-masonry -->
+
+<?php echo end_sec_fixedCenteredTitle(); ?>
+
+
+
+<!-- stats -->
+
+<?php echo sec_fixedCenteredTitle(
+  '<b class="slide-from-left">This is</b>
+   <b class="slide-from-right">Vassar.</b>',
+   '<p class="section-intro-text">
+
+Vassar is from everywhere. World-class educations are built on world-wide communities. Our students come from 44 states and 17 countries. Nearly one in ten come from foreign countries; nearly half will visit them while at Vassar. Over half of Vassar students receive financial aid.
+
+<p><strong class="cta-link">Learn more</strong></p>
+
+   </p>',
+  'sec-fixedCenteredTitle theme-burgundy',
+  'mw-4',
+    ['bg-image-url' => 'journey/0256-19-10-ja-library-lawn-vassar-vb-038.jpg',
+    'bg-image-alt' => 'alt text',
+    'css' => '
+      --section-title-size: 14vw;
+      --section-title-faded-opacity: 0.1;
+      --title-container-bg-beforeContent: rgba(0,0,0,0.4);
+      --section-bg-image-opacity: 0.1;
+      '
+    ]
+); ?>
+
+<div class="xlayout-masonry animation-group loose-grid stat-grid">
+  <!--<div class="grid-sizer"></div>-->
+
+  <?php echo item_statIcon(
+    '2,435',
+    'students',
+    'people-group',
+    'animation-item masonry-item'
   ); ?>
-<?php echo end_item_col(); ?>
 
-<?php echo item_col('animation-item'); ?>
-  <?php echo item_imageCard(
-    get_icon('play').'Card title',
-    null,
-    'apple.com',
-    ['url' => 'https://images.unsplash.com/photo-1652703747774-558a10faacc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80', 'alt' => 'asdfasdfds' ],
-    'text-at-bottom',
-    ['hlevel' => 4]
+  <?php echo item_statIcon(
+    '340',
+    'faculty',
+    'person-chalkboard',
+    'animation-item masonry-item'
   ); ?>
-<?php echo end_item_col(); ?>
 
-<?php echo item_col('animation-item'); ?>
-  <?php echo item_imageCard(
-    get_icon('play').'Card title',
-    null,
-    'apple.com',
-    ['url' => 'https://images.unsplash.com/photo-1652703747774-558a10faacc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80', 'alt' => 'asdfasdfds' ],
-    'text-at-bottom',
-    ['hlevel' => 4]
+  <?php echo item_statIcon(
+    '$54K',
+    'average aid award',
+    'coins',
+    'animation-item masonry-item'
   ); ?>
-<?php echo end_item_col(); ?>
 
+  <?php echo item_statIcon(
+    '51',
+    'majors',
+    'book',
+    'animation-item masonry-item'
+  ); ?>
+
+  <?php echo item_statIcon(
+    '8:1',
+    'students to faculty',
+    'users',
+    'animation-item masonry-item'
+  ); ?>
+
+  <?php echo item_statIcon(
+    '27',
+    'varsity teams',
+    'person-running',
+    'animation-item masonry-item'
+  ); ?>
+
+  <?php echo item_statIcon(
+    '170',
+    'student orgs',
+    'screwdriver-wrench',
+    'animation-item masonry-item'
+  ); ?>
+
+</div><!-- end layout-masonry -->
+
+<?php echo end_sec_fixedCenteredTitle(); ?>
+
+<!-- end stats -->
+
+
+
+<!-- visit -->
+
+<?php echo sec_fixedCenteredTitle(
+  '<b class="slide-from-left">Visit before</b>
+   <b class="slide-from-right">you visit.</b>',
+   '<p class="section-intro-text">
+All Vassar students are surrounded by an environment designed to spark something amazing—a place where greatness can spring to life. Explore virtual visiting opportunities today and learn more about in-person visits.
+   </p>
+
+<p><strong class="cta-link">Visiting the Vassar campus</strong></p>
+   ',
+  'sec-fixedCenteredTitle theme-dark-burgundy featured-video',
+  'mw-4',
+    ['bg-image-url' => 'visit/0216-18-10-jsu-fall-drone-vassar-R01895.jpg',
+    'bg-image-alt' => 'alt text',
+    'css' => '
+      --section-title-size: 14vw;
+      --section-title-faded-opacity: 0.1;
+      --title-container-bg-beforeContent: rgba(0,0,0,0.4);
+      --section-bg-image-opacity: 0.1;
+      '
+    ]
+); ?>
+
+<div class="embed-responsive embed-responsive-16by9 ratio ratio-16x9">
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/531201548?h=eca7efdc85" width="640" height="360" frameborder="0" allowfullscreen></iframe>
 </div>
 
-<?php echo end_sec_wideContent(); ?>
+
+
+
+<?php echo end_sec_fixedCenteredTitle(); ?>
+
+<!-- end visit -->
+
+
+
+<!-- fav places -->
+
+<?php echo sec_fixedCenteredTitle(
+  '<b class="slide-from-left">Our favorite</b>
+   <b class="slide-from-right">places.</b>',
+   '<p class="section-intro-text">
+
+Vassar is extremely proud of our 1,000-acre campus. There are buildings that showcase classic architecture blended seamlessly with modern, cutting-edge facilities.
+
+   </p>',
+  'sec-fixedCenteredTitle theme-charcoal',
+  'mw-4',
+    ['bg-image-url' => 'visit/0256-19-10-ja-library-lawn-vassar-vb-038.jpg',
+    'bg-image-alt' => 'alt text',
+    'css' => '
+      --section-title-size: 14vw;
+      --section-title-faded-opacity: 0.1;
+      --title-container-bg-beforeContent: rgba(0,0,0,0.4);
+      --section-bg-image-opacity: 0.1;
+      '
+    ]
+); ?>
+
+  <div class="layout-masonry layout-masonry-33 animation-group">
+
+    <div class="grid-sizer"></div>
+
+  <?php echo item_imageCard(
+    get_icon('play').'Shakespeare Garden',
+    null,
+    'apple.com',
+    ['url' => 'fav-places/0056-21-06-kr-campus-vassar-0006.jpg', 'alt' => 'asdfasdfds' ],
+    'animation-item masonry-item text-at-bottom'
+  ); ?>
+
+  <?php echo item_imageCard(
+    get_icon('play').'Thompson Memorial Library',
+    null,
+    'apple.com',
+    ['url' => 'fav-places/0212-18-10-kr-library-fall-vassar-3819.jpg', 'alt' => 'asdfasdfds' ],
+    'animation-item masonry-item text-at-bottom',
+    ['hlevel' => 4]
+  ); ?>
+
+  <?php echo item_imageCard(
+    get_icon('play').'Gordon Commons',
+    null,
+    'apple.com',
+    ['url' => 'fav-places/0195-19-11-kwe-gordon-vassar-1644.jpg', 'alt' => 'asdfasdfds' ],
+    'animation-item masonry-item text-at-bottom',
+    ['hlevel' => 4]
+  ); ?>
+
+  <?php echo item_imageCard(
+    get_icon('play').'Bridge for Laboratory Sciences',
+    null,
+    'apple.com',
+    ['url' => 'fav-places/110619_160over90_Vassar_5483.jpg', 'alt' => 'asdfasdfds' ],
+    'animation-item masonry-item text-at-bottom',
+    ['hlevel' => 4]
+  ); ?>
+
+  <?php echo item_imageCard(
+    get_icon('play').'The Ecological Preserve',
+    null,
+    'apple.com',
+    ['url' => 'fav-places/farm-ramble-walking-Tour-1709-sal-1.jpg', 'alt' => 'asdfasdfds' ],
+    'animation-item masonry-item w-66 text-at-bottom',
+    ['hlevel' => 4]
+  ); ?>
+
+  <?php echo item_imageCard(
+    get_icon('play').'Student Picks',
+    null,
+    'apple.com',
+    ['url' => 'fav-places/studio.jpg', 'alt' => 'asdfasdfds' ],
+    'animation-item masonry-item text-at-bottom',
+    ['hlevel' => 4]
+  ); ?>
+
+</div><!-- end layout-masonry -->
+
+<?php echo end_sec_fixedCenteredTitle(); ?>
+
+<!-- fav places -->
+
+
 
 
 
 <div class="buffer"></div>
-
-
-<?php echo sec_wideContent(
-  'Wide content',
-  '<p class="section-intro-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>',
-  null,
-  'mw-4',
-    ['bg-image-url' => 'https://images.unsplash.com/photo-1652703747774-558a10faacc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80',
-    'bg-image-alt' => 'alt text',
-    'title_classes' => 'animate-when-content-appears animation-zoom-in'
-  ]
-); ?>
-
-<div class="grid cols-3 gap-2 animation-group animate-when-content-appears animation-zoom-in">
-
-  <?php echo item_iconButton(
-    'car',
-    'Students are allowed to have cars on campus.',
-    'vassar.edu',
-    'btn-card animation-item'
-  );
-  ?>
-
-  <?php echo item_iconButton(
-    'car',
-    'Vassar does not currently require SAT or ACT scores.',
-    'vassar.edu',
-    'btn-card animation-item'
-  );
-  ?>
-
-  <?php echo item_iconButton(
-    'car',
-    'Vassar meets all demonstrated financial need for all four years.',
-    'vassar.edu',
-    'btn-card animation-item'
-  );
-  ?>
-
-  <?php echo item_iconButton(
-    'car',
-    'Housing is guaranteed for all four years.',
-    'vassar.edu',
-    'btn-card animation-item'
-  );
-  ?>
-
-  <?php echo item_iconButton(
-    'car',
-    'Interviews aren’t required, but they’re informative.',
-    'vassar.edu',
-    'btn-card animation-item'
-  );
-  ?>
-
-  <?php echo item_iconButton(
-    'car',
-    'We award credit for AP scores of 4/5 and IB scores of 5/6/7.',
-    'vassar.edu',
-    'btn-card animation-item'
-  );
-  ?>
-
-
-</div>
-
-<?php echo end_sec_wideContent(); ?>
-
-
-
-
-
-
-
-<div class="buffer"></div>
-
 
 <?php echo site_footer(); ?>
