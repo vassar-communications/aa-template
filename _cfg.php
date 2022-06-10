c18f66a2c0ba6f132b3551de34dacfa984b8c698
csilverman <chris@csilverman.com>
2022-06-10 11:35:11 -0400

    New changes

--- _cfg.php
+++ _cfg.php
@@ -1,3 +1,9 @@
 <?php
-$project_paths['main_project_root'] = $_SERVER['DOCUMENT_ROOT'];
-?>
+
+// I'm including all paths in one variable so they can be
+// easily imported into functions if needed
+//
+$project_paths = array(
+  'main_project_root' => '/Users/chsilverman/Local Sites/hario/app/public',
+  'public_path' => ''
+);
---
+++ x_cfg.php
@@ -0,0 +1,3 @@
+<?php
+$project_paths['main_project_root'] = $_SERVER['DOCUMENT_ROOT'];
+?>
