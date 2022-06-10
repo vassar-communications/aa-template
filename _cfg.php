6ab48526f94f4f04b3b6c0216b7a5296b0c7ad8e
csilverman <chris@csilverman.com>
2022-06-10 11:47:57 -0400

    Small changes

--- _cfg.php
+++ _cfg.php
@@ -2,7 +2,6 @@
 
 // I'm including all paths in one variable so they can be
 // easily imported into functions if needed
-//
 $project_paths = array(
   'main_project_root' => '/Users/chsilverman/Local Sites/hario/app/public',
   'public_path' => ''
