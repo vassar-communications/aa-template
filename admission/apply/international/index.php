a71dcef40814eb3adf3ffedfd65a048290fe66cb
cyatsuk-ba <105189781+cyatsuk-ba@users.noreply.github.com>
2022-06-14 19:53:17 +0900

    Content Update

--- admission/apply/international/index.php
+++ admission/apply/international/index.php
@@ -40,12 +40,12 @@ $page_classes = "";
 ); ?>
 
 <?php echo item_col('equal'); ?>
-<h3>Vassar believes in building a vibrant community of global citizens. </h3>
+<h3 class="display-4">Vassar believes in building a vibrant community of global citizens. </h3>
 <p>Vassar students come from more than 79 countries around the world and international students make up more than 10% of our community.</p>
 <?php echo end_item_col(); ?>
 <?php echo item_col('equal'); ?>
 
-<img src="" />
+<img src="https://vassartest.chuckyatsuk.com/img/apply/international/0074_15_08_KR_0088.jpg" />
 <?php echo end_item_col(); ?>
 
 <?php echo end_sec_hasColumns(); ?>
@@ -62,7 +62,7 @@ $page_classes = "";
 ); ?>
 
 
-<h3>Who is considered an international student?</h3>
+<h3 class="display-4">Who is considered an international student?</h3>
 <p>When applying for admission to Vassar, anyone who is not a citizen or a permanent resident of the United States is considered an international student. If you have applied for a green card (permanent resident status) but have not received it by the time you apply, then you are considered an international student.</p>
 <p><strong>For our general admission requirements for all students, please review the following pages:</strong><br/>
 <p>
@@ -80,7 +80,7 @@ $page_classes = "";
 ); ?>
 
 
-<h3>English Language Proficiency</h3>
+<h3 class="display-4">English Language Proficiency</h3>
     <p>If English is either your native or heritage language, or the primary language of instruction in the secondary school(s) you have attended for the last three years, you do not need to submit English language proficiency exam results.</p>
     <p>For all other applicants, we require one of the following tests, without exceptions, as proof of language proficiency:</p>
     <p>Click on the exam type below for complete details</p>
@@ -130,52 +130,34 @@ $page_classes = "";
         </div>
     </div>
 </div>
+<p>Vassar reviews only the single-sitting highest score for all English Proficiency exams.
+</p>
 
-<?php echo end_sec_regularContent(); ?>
+<hr class="my-5">
 
+<h3 class="display-4">School Materials</h3>
 
-<?php echo sec_regularContent(
-    null,
-    'theme-cream',
-    ''
-); ?>
+<p>We have students who apply from dozens of different countries each year and receive academic records in a variety of formats. In general, your transcript should be a complete record of your coursework and grades from each marking period of secondary school, which is certified by your school. If you have taken required national/international exams during secondary school (ie. GCSE, IB MYP, CBSE board exams), these results should be included with your transcript. Any terminal high school exams (ie. A levels, IB exams, French bacc, CBSE) will be required for enrollment.</p>
 
-<h3>Your application should include:</h3>
-    <ul>
-        <li>A transcript – a complete, certified record of your coursework and grades from each marking period of secondary school</li>
-        <li>Exam results, i.e. GCSE, IB, MYP, CBSE board exams</li>
-        <li>Terminal high school exams, i.e. A levels, IB exams, French bacc, CBSE</li>
-        <li>Recommendations – if translated from the language of instruction to English, please provide a signed copy of the original as well as the translation</li>
-    </ul>
-    <p>All supporting documents should be in English. If your school does not provide a transcript in English, please provide both a certified copy of the original transcript and a translated copy in English.</p>
+<p>Additionally, all of your supporting documents should be in English. If your school does not provide a transcript in English, we need both a certified copy of the original transcript and a translated copy in English. The translation should be completed by an agency, but can be done by a school official or English teacher, if an agency translation presents a financial hardship. Also, if your recommendations are translated from the language of instruction to English, please provide a signed copy of the original as well as the translation.</p>
 
+<hr class="my-5">
 
-<?php echo end_sec_regularContent(); ?>
-
-
-
-<?php echo sec_regularContent(
-    null,
-    '',
-    ''
-); ?>
-
-
-<h3>Interviews</h3>
+<h3 class="display-4">Interviews</h3>
     <p>We recommend that applicants who submit the TOEFL/IELTS also complete an interview as an additional way to demonstrate English language proficiency; students who submit Duolingo will have the interview portion of the exam reviewed as part of their application.</p>
 <p>Vassar also accepts interviews from <a class="fix" href="">InitialView</a> and <a class="fix" href="">Vericant</a>. While international applicants can’t request alumnae/i or student interviews, if we have interviewers in your area, we will reach out to you to set up an interview.</p>
-<hr/>
-<h3>Financial Aid</h3>
+<hr class="my-5">
+<h3 class="display-4">Financial Aid</h3>
 <p>Vassar meets the full demonstrated financial need of all students for all four years, but we are need-aware when reviewing applications from international students. Students who are neither U.S. citizens nor permanent residents who wish to be considered for financial aid at any time during their four years at Vassar must apply at the same time that they apply for admission. Those students admitted without financial aid in the first year are not eligible to receive Vassar funding during their years at the college. Financial aid is not available for international transfer applicants.</p>
-<hr/>
-<h3>U.S. Citizens/Permanent Resident Attending School Abroad</h3>
+<hr class="my-5">
+<h3 class="display-4">U.S. Citizens/Permanent Resident Attending School Abroad</h3>
 <p>If you are a U.S. citizen or U.S. permanent resident attending school outside of the United States, your admission decision will be made without regard to your financial situation, but your application will be reviewed in the context of the international school you attend.</p>
 
 <p>If English is not your first language or the language of instruction for your past three years of secondary school, we still require you to submit English proficiency exam results.</p>
 
-<hr/>
+<hr class="my-5">
 
-<h3>International Student Services</h3>
+<h3 class="display-4">International Student Services</h3>
 <p>The <a class="fix" href="">Office of International Services (OIS)</a> offers a full range of resources for international students to make the Vassar experience as enjoyable and successful as possible. International students can also join the student-run Vassar International Students Association (VISA).</p>
 <?php echo end_sec_regularContent(); ?>
 
