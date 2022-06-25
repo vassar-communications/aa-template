e78d5e24acffd11576f4071347f577bf2428ecb8
cyatsuk-ba <105189781+cyatsuk-ba@users.noreply.github.com>
2022-06-25 23:48:46 +0900

    initial sass cleanup

--- admission/inc/nav-elements/admission-nav.php
+++ admission/inc/nav-elements/admission-nav.php
@@ -1,6 +1,6 @@
-<li class="animation-item"><a href="/admission/explore/">Explore Vassar</a></li>
-<li class="animation-item"><a href="/admission/apply/">Apply</a></li>
-<li class="animation-item"><a href="/admission/visit/">Visit Campus</a></li>
-<li class="animation-item"><a href="/admission/financial-aid/">Financial Aid</a></li>
-<li class="animation-item"><a href="/admission/quick-facts/">Quick Facts</a></li>
-<li class="animation-item"><a href="/admission/connect/">Connect</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/explore/">Explore Vassar</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/apply/">Apply</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/visit/">Visit Campus</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/financial-aid/">Financial Aid</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/quick-facts/">Quick Facts</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/connect/">Connect</a></li>
--- admission/inc/nav-elements/admission-visit-nav.php
+++ admission/inc/nav-elements/admission-visit-nav.php
@@ -1,3 +1,3 @@
-<li class="animation-item active"><a href="/admission/visit/in-person/">In-Person Visits</a></li>
-<li class="animation-item"><a href="/admission/visit/virtual-programs/">Virtual Programs</a></li>
-<li class="animation-item"><a href="/admission/visit/group-visits/">Group Visits</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/visit/in-person/">In-Person Visits</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/visit/virtual-programs/">Virtual Programs</a></li>
+<li class="nav-item"><a class="nav-link" href="/admission/visit/group-visits/">Group Visits</a></li>
