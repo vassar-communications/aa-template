21cc1d64ad2793df8a1c2463705105d72e59d101
cyatsuk-ba <105189781+cyatsuk-ba@users.noreply.github.com>
2022-07-14 23:20:43 +0900

    functions.php fix

--- functions.php
+++ functions.php
@@ -10,126 +10,8 @@ include($project_paths['main_project_root'].'/core/template-parts/header.inc');
 include($project_paths['main_project_root'].'/core/template-parts/footer.inc');
 include($project_paths['main_project_root'].'/core/template-parts/tmpfooteralumni.inc');
 
-<<<<<<< Updated upstream
-/* THIS NEEDS TO BE INTEGRATED */
-
-function word_cycler($word_selector_id) {
-
-  $speed = 1600;
-
-    $list = '
-    "New York",
-    "Egypt",
-    "Nigeria",
-    "Maryland",
-    "Puerto Rico",
-    "Jamaica",
-    "California",
-    "Ecuador",
-    "India",
-    "Ohio",
-    "Japan"
-  ';
-
-    return <<<TMP
-<style>
-@keyframes word-fade {
-  0% { opacity: 0; }
-  5% { opacity: 0; }
-  50% { opacity: 1; }
-  95% { opacity: 0; }
-  100% { opacity: 0; }
-}
-
-#$word_selector_id {
-  animation-name: word-fade;
-  animation-duration: {$speed}ms;
-  animation-iteration-count: infinite;
-}
-
-</style>
-
-<script>
-$(document).ready(function () {
-
-  var arr = [$list];
-
-  // run through the array forever
-  (function recurse(counter) {
-      var color = arr[counter];
-      $('#$word_selector_id').delay('$speed').html(color);
-      // delete the value to save memory
-      delete arr[counter];
-      // add the value at the end of the array
-      arr.push(color);
-
-      // run it again for the next number
-      setTimeout(function() {
-          recurse(counter + 1);
-      }, $speed);
-  // start it for the first number.
-  })(0);
-
-});
-</script>
-
-TMP;
-
-}
-
-
-
-function vassar_is_awesome($classes) {
-    $our_qualities = '<ul><li>unafraid to question</li><li>a story of strength and substance</li><li>belonging and thriving</li><li>small but mighty</li><li>courage of conviction</li><li>a remarkable sense of place</li></ul>';
-
-    return <<<TMP
-<div class="awesome_ticker $classes">
-  <div id="div1">
-$our_qualities
-  </div>
-  <div id="div2">
-$our_qualities
-  </div>
-</div>
-TMP;
-
-}
-
-
-
-
-function flipcard_stat(
-    $title,
-    $value,
-    $reverse_content,
-    $class=null,
-    $css=null
-) {
-
-
-    if($css) { $css = sprintf('style="%s"', $css); }
-
-    return <<<TMP
-<div class="flip-card $class" $css>
-  <div class="card-content">
-    <div class="card__face card__face--front">
-      <p class="flipcard__title"><strong>$title</strong></p>
-      <p class="flipcard__value">$value</p>
-    </div>
-    <div class="card__face card__face--back">
-      $reverse_content
-    </div>
-  </div>
-</div>
-TMP;
-
-}
-
-
-=======
 include($project_paths['main_project_root'].'/core/template-parts/alums-topLevelNav.inc');
 include($project_paths['main_project_root'].'/core/template-parts/admission-topLevelNav.inc');
->>>>>>> Stashed changes
 
 
 
@@ -189,12 +71,6 @@ include($project_paths['main_project_root'].'/core/sections/content_sections/wid
 include($project_paths['main_project_root'].'/core/sections/content_sections/fixedCenteredTitle.inc');
 include($project_paths['main_project_root'].'/core/sections/content_sections/fixedCenteredTitle-masthead.inc');
 include($project_paths['main_project_root'].'/core/sections/content_sections/fixedCenteredTitle-masthead_vid.inc');
-<<<<<<< Updated upstream
-include($project_paths['main_project_root'].'/core/sections/content_sections/fixedCenteredTitle-masthead_vid-give.inc');
-
-
-=======
->>>>>>> Stashed changes
 include($project_paths['main_project_root'].'/core/sections/content_sections/news-carousel.inc');
 include($project_paths['main_project_root'].'/core/sections/content_sections/alums-carousel.inc');
 include($project_paths['main_project_root'].'/core/sections/content_sections/inTheMedia-carousel.inc');
