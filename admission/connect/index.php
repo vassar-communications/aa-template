21ae9a044632afa50b291c16ecbd89ab74d2223f
csilverman <chris@csilverman.com>
2022-07-17 04:05:20 -0400

    Working on Connect page, added icon-link component

--- admission/connect/index.php
+++ admission/connect/index.php
@@ -20,7 +20,7 @@ $page_classes = "";
 
 <?php echo hamburger_navigation(); ?>
 
-<?php echo page_textmasthead('theme-dark-burgundy'); ?>
+<?php echo page_partialmasthead($placeholder_img_banner); ?>
 
 <?php echo breadcrumbNav('theme-cream'); ?>
 <?php include($project_paths['main_project_root'].'/admission/inc/nav-elements/admission-connect-breadcrumb.php');?>
