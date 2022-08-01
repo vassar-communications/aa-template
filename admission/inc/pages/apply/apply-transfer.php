ed42591cddfe6a0ac4b164210c49dcb585e7c820
cyatsuk-ba <105189781+cyatsuk-ba@users.noreply.github.com>
2022-08-01 20:47:00 +0700

    Updates to admission cards

--- admission/inc/pages/apply/apply-transfer.php
+++ admission/inc/pages/apply/apply-transfer.php
@@ -1,11 +1,11 @@
 <?php echo item_cardWithText(
     'Transfer Applicants',
     '/admission/apply/transfer/',
-    ['url' => $admission_img_path.'apply/transfer/0137_14_11_KR_0003.jpg', 'alt' => 'Student reading a sheet of paper in a classroom' ],
+    ['url' => $admission_img_path.'sitewide-cards/0055-16-05-kr-spring-vassar-0011.jpg', 'alt' => 'Student walking and looking at phone with ear buds' ],
     'related-topics card--withParagraph card-has-cta-icon card-is-link'
 ); ?>
 
-    <p>See the checklist of requirements to transfer to Vassar.</p>
+<p>See the checklist of requirements to transfer to Vassar.</p>
 
 <?php echo end_item_cardWithText(
     null,
