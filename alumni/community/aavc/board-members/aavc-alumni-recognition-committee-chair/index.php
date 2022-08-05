8267476c1b32b72e8f74129076015aa532dabdc6
Jeff Macaluso <samacaluso@vassar.edu>
2022-08-05 14:25:41 -0400

    AAVC Board and Groups
    Bennet, apostrophes, links

--- alumni/community/aavc/board-members/aavc-alumni-recognition-committee-chair/index.php
+++ alumni/community/aavc/board-members/aavc-alumni-recognition-committee-chair/index.php
@@ -8,9 +8,9 @@ include($project_paths['main_project_root'].'/functions.php');
 
 
 $page_info = '{
-  "page_title":"Alumnae/i Recognition Committee Chair",
+  "page_title":"AAVC Board Director",
   "page_classes":"",
-  "feature_image":"/alumni/assets/img/alumni-community/aavc/board/pullman-amy-aavc-alum-001.jpg"
+  "feature_image":"/alumni/assets/img/alumni-community/aavc/board/bennett-maybelle-taylor-aavc-alum-001.jpg"
 }';
 
 /* === */
@@ -31,18 +31,18 @@ $page_info = json_decode($page_info, true);
 <?php /*  PAGE CONTENT === */ ?>
 
 <?php echo sec_fullBleedImageColumn(
-    'Amy Pullman ’71',
-    '/alumni/assets/img/alumni-community/aavc/board/pullman-amy-aavc-alum-001.jpg',
+    'Maybelle Taylor Bennett ’70',
+    '/alumni/assets/img/alumni-community/aavc/board/bennett-maybelle-taylor-aavc-alum-001.jpg',
     'image-is-first theme-cream',
     '',
-    ['img_alt_text' => 'Anne Green']
+    ['img_alt_text' => 'Maybelle Taylor Bennett ’70']
 ); ?>
 
   <ul class="clean-list-with-borders">
-    <li>Alumnae/i Recognition Committee Chair</li>
+    <li>Alumnae/i Recognition Committee Member and Career Networking Committee Member</li>
     <li>(she/her/hers)</li>
-    <li>Virginia</li>
-    <li>Term of Service: 2020–2024</li>
+    <li>Washington, D.C.</li>
+    <li>Term of Service: 2021–2025</li>
   </ul>
 
 <?php echo end_sec_fullBleedImageColumn(); ?>
@@ -53,7 +53,9 @@ $page_info = json_decode($page_info, true);
     '',
     ''
 ); ?>
-<p>Class Agent, Class of 1971, 2017–present; member, Alumnae/i Recognition Committee, 2016–present; President, Class of 1971, 2011–present; volunteer, Career Advisory Network, 2008–present; member, Reunion Steering Committee, 2016–2019; member, AAVC Nominating Committee, 2010–2013; member, Club Admissions Committee, 2007–2009; Reunion Co-Chair, Class of 1971, 2000–2001; President, Class of 1971, 1995–1999; Reunion Chair, Class of 1971, 1995–1996; Vice President, Class of 1971, 1991–1995; President, Class of 1971, 1986–1991; volunteer, Class of 1971, 1982; Vice President, Class of 1971, 1976–1986.</p>
+<p>Class agent, Class of 1970, 2018–present; member, Club Admission Committee, 1997–2009; notewriter/phoner, Annual Fund, 2005–2006; member, AAVC Nominating Committee, 2002–2005; panelist, 30th Anniversary Celebration of the Africana Studies Program at Vassar, 2001.
+
+</p>
 <?php echo end_sec_regularContent(); ?>
 
 <?php echo sec_regularContent(
@@ -62,7 +64,7 @@ $page_info = json_decode($page_info, true);
     '',
     ''
 ); ?>
-<p>Assistant Clinical Professor of Pediatrics, The George Washington University School of Medicine, 2000–present; pediatrician, Pullman Associates Pediatrics (Panda Pediatrics), 1999–present; staff pediatrician, Sibley Memorial Hospital, 1984–1999.</p>
+<p>Director, Howard University Community Association, Howard University, 1996–2019; member, Board of Directors, Douglass Community Land Trust, 2020–present; pastor, Covenant Christian Community 2013–present; assistant for community relations and planning, Howard University, 1991–1996; commissioner and chair, Zoning Commission of the District of Columbia, 1982–1998; member, Leadership Washington, 1991–present; member, Lambda Alpha Land Economics Society, 1984–present; member of the Board of Directors of the D.C. Building Industry Association, 2000–2015; director of research, Coalition on Human Needs, 1984–1990; director of research, Working Group for Community Development Reform, 1978–1981.</p>
 <?php echo end_sec_regularContent(); ?>
 
 
