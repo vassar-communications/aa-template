94bd0780f602b26acce8ca21c1202c1ec5313804
csilverman <chris@csilverman.com>
2022-08-07 19:12:06 -0400

    Masthead and breadcrumb mobile fixes

--- alumni/community/alumni-groups/index.php
+++ alumni/community/alumni-groups/index.php
@@ -58,7 +58,7 @@ $page_info = json_decode($page_info, true);
 <div class="animate-when-content-appears animation-slide-up">
     <p>Affinity groups strengthen alumni communities that center shared experiences, activities, and identities. These communities include athletic teams, singing groups, LGBTQ+, ALANA orgs, veterans, and First Gen alums (among others).</p>
     <!-- <p>If you are interested in participating in affinity groups, reach out to Sharif Rosen, Associate Director for Multicultural and Affinity Engagement at   <a href="mailto:srosen@vassar.edu">srosen@vassar.edu</a> or <a href="tel:845-437-5543">(845) 437-5443</a>.</p> -->
-    <ul class="linked-list">
+    <ul class="linked-list mt-4">
         <li><a href="https://www.vassar.edu/social-media-alumni#affinity-groups">Follow Affinity Groups on Social</a></li>
         <li><a href="http://connect.vassar.edu/volunteer">Become an Affinity Group Volunteer</a></li>
     </ul>
@@ -76,8 +76,8 @@ $page_info = json_decode($page_info, true);
 <div class="animate-when-content-appears animation-slide-up">
 <p>Everyone who matriculates at Vassar is a member of a Vassar Class. Classes plan reunions and mini-reunions, manage social media pages, reach out to one another about supporting the college, and enjoy each other’s updates in the Class notes section of <em>The Vassar Quarterly</em> magazine. Classes last for a lifetime and represent the largest organizing alumni community.</p>
 
-    <p>If you are interested in participating in Class activities, reach out to Diana Jedlicka at <a href="mailto:djedlicka@vassar.edu">djedlicka@vassar.edu</a> or <a href="tel:845-437-5398">(845)437-5398</a>.</p>
-    <ul class="linked-list">
+    <p>If you are interested in participating in Class activities, reach out to Diana Jedlicka at <a href="mailto:djedlicka@vassar.edu">djedlicka@vassar.edu</a> or <?php echo phone_number('845-437-5398'); ?>.</p>
+    <ul class="linked-list mt-4">
         <li><a href="/alumni/calendar/reunions/">Explore Upcoming Reunions</a></li>
         <li><a href="https://www.vassar.edu/social-media/class-by-year">Follow Classes on Social</a></li>
         <li><a href="https://securelb.imodules.com/s/1654/2/16/interior-ai.aspx?sid=1654&gid=2&pgid=3956&cid=6461&post_id=0">Become a Class Volunteer</a></li>
@@ -98,7 +98,7 @@ $page_info = json_decode($page_info, true);
 <div class="animate-when-content-appears animation-slide-up">
 <p>Vassar Clubs serve as a catalyst to bring together the more than 41,000 Vassar alumni around the world, fostering lasting connections beyond the College’s gates.</p>
     <p>If you are interested in participating in Club activities or starting a Club, reach out to Cheryl McKeever at <a href="mailto:cmckeever@vassar.edu">cmckeever@vassar.edu</a> or <a href="tel:845-437-5408">845-437-5408</a>.</p>
-    <ul class="linked-list">
+    <ul class="linked-list mt-4">
         <li><a href="https://connect.vassar.edu/s/1654/2/16/interior-ai.aspx?sid=1654&gid=2&pgid=5161">Explore Upcoming Club Events</a></li>
         <li><a href="https://www.vassar.edu/social-media-alumni#regional-clubs">Follow Vassar Clubs on Social</a></li>
         <li><a href="http://connect.vassar.edu/volunteer">Become a Club Volunteer</a></li>
