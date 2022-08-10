11f4e22615e71e4776ba3b1630803caedd66fcb1
csilverman <chris@csilverman.com>
2022-08-09 23:16:07 -0400

    Quick fix

---
+++ alumni/inc/_footer-mod-optional.php
@@ -0,0 +1,10 @@
+<div class="button-bar">
+  <ul id="block-fixedalumsmyaccountdirectory">
+    <li>
+      <a href="https://connect.vassar.edu/s/1654/alumni/index.aspx?sid=1654&amp;gid=2&amp;pgid=95&amp;cid=271" class="btn btn-primary">My Account</a>
+    </li>
+    <li>
+      <a href="https://connect.vassar.edu/s/1654/2/16/interior-ai.aspx?sid=1654&amp;gid=2&amp;pgid=6&amp;cid=41#/Search/Simple" class="btn btn-primary">Alumni Directory</a>
+    </li>
+  </ul>
+</div>
