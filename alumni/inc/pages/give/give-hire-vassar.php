506aa56bfe571f76df5d12244e8c8a265038d584
Jeff Macaluso <samacaluso@vassar.edu>
2022-08-12 16:34:27 -0400

    alumni/inc/pages/give/ files
    Alt text

--- alumni/inc/pages/give/give-hire-vassar.php
+++ alumni/inc/pages/give/give-hire-vassar.php
@@ -1,7 +1,7 @@
 <?php echo item_cardWithText(
     'Hire Vassar',
     '/alumni/give/hire-vassar/',
-    ['url' => '/alumni/assets/img/alumni-give/hire/0006-19-01-kr-scc-reception-vassar-0977.jpg', 'alt' => 'replacealttext' ],
+    ['url' => '/alumni/assets/img/alumni-give/hire/0006-19-01-kr-scc-reception-vassar-0977.jpg', 'alt' => 'mentor in a group setting speaking with another individual' ],
     'related-topics card--withParagraph card-has-cta-icon card-is-link'
 ); ?>
 
