323ff472e1181d6666c40176636e0664c3ef5778
Jeff Macaluso <samacaluso@vassar.edu>
2022-08-14 21:46:57 -0400

    Vassar Network Related Topics card
    alums to alumni

--- alumni/inc/pages/resources/resources-vassar-network.php
+++ alumni/inc/pages/resources/resources-vassar-network.php
@@ -4,7 +4,7 @@
     ['url' => '/alumni/assets/img/alumni-resources/vassar-network/0039-22-06-cs-reunion-vassar-390.jpg', 'alt' => 'Two adults and a child waving on Vassar Campus' ],
     'related-topics card--withParagraph card-has-cta-icon card-is-link'
 ); ?>
-    <p>Join the global Vassar community and connect with alums around the world.</p>
+    <p>Join the global Vassar community and connect with alumni around the world.</p>
 <?php echo end_item_cardWithText(
     null,
 ); ?>
