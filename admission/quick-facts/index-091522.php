4da6d2a1b0240d2327da8a5ab32ff1a79d62de06
cyatsuk-ba <105189781+cyatsuk-ba@users.noreply.github.com>
2022-09-15 07:25:29 -0400

    Updates to Quick Facts Page

--- admission/quick-facts/index-091522.php
+++ admission/quick-facts/index-091522.php
@@ -127,11 +127,89 @@ $page_info = json_decode($page_info, true);
 
 
 
+<?php echo sec_regularContent(
+    'What’s in Your Application',
+    'has-bg quad-pattern theme-extralightgray',
+    ''
+); ?>
+
+<p class="intro-text">Your application can contain a number of items, some of which are optional.</p>
+
+<?php echo end_sec_regularContent(); ?>
+
+<?php echo sec_hasColumns(
+    'Required',
+    '1',
+    '',
+    'theme-extralightgray',
+    '',
+    ['title_level' => '3']
+); ?>
+
+<?php echo item_col(); ?>
+
+<ul class="style-checklist">
+    <li><strong class="h3">Early Decision Agreement</strong><br>
+        (Early Decision Applicants only)
+    </li>
+    <li><strong class="h3">School Report and High School Transcript</strong><br>
+        To be completed and sent by your guidance counselor.
+    </li>
+    <li><strong class="h3">Mid-Year Grade Report</strong><br>
+        Vassar requests each applicant’s most recent grade report to be sent by their school counselor.
+
+        <ul>
+            <li><strong>Early Decision I or II applicants:</strong> Vassar requires a first quarter/trimester grade report, which should be sent as soon as it becomes available, and first semester grades as soon as they become available.</li>
+            <li><strong>Regular Decision applicants:</strong> A mid-year report with first semester grades is required when available, typically by February 1.</li>
+        </ul>
+    </li>
+    <li><strong class="h3">Teacher Evaluations/Recommendation Letters </strong><br>
+        Vassar requires two recommendations: one from your guidance counselor or school advisor, and one from a teacher in a core academic subject. If you wish, you may submit a third letter, either from another teacher or from someone else in your life who can share pertinent information.
+    </li>
+</ul>
+
+<?php echo end_item_col(); ?>
+
+
+<?php echo end_sec_hasColumns(); ?>
+
+
+<?php echo sec_hasColumns(
+    'Optional',
+    '2',
+    '',
+    'theme-extralightgray border-top mw-inner-3',
+    '',
+    ['title_level' => '3']
+); ?>
+
+<?php echo item_col(); ?>
+<?php echo item_iconItem(
+    'Interviews',
+    'people-arrows-left-right',
+    'icon-on-left'
+); ?>
+<p>Off-campus interviews are available for first-year applicants. These are conducted all over the world by Vassar alumni and are available from November through February. Interviews are not required, but they are a great way to learn about Vassar from a new perspective. Not having an interview will not affect your admission decision. Requests for alumni interviews will not be processed until we receive your application.</p>
+<?php echo end_item_iconItem(); ?>
+<?php echo end_item_col(); ?>
+
+<?php echo item_col(); ?>
+<?php echo item_iconItem(
+    'Your Space',
+    'child-reaching',
+    'icon-on-left'
+); ?>
+<p>“Your Space” is an optional section of our application...so don’t stress over it! This is a chance for you to show the admission committee something else about yourself. There is no “right” answer on what you should submit. For example, in the past, applicants have sent poetry, cartoons, art projects, photography, collages, short stories, videos, and short films. It is your space, so if you choose to complete it, send something that is a reflection of you!</p>
+<?php echo end_item_iconItem(); ?>
+<?php echo end_item_col(); ?>
+
+
+<?php echo end_sec_hasColumns(); ?>
 
 <a id="standardized-testing"></a>
 <?php echo sec_regularContent(
     'SAT/ACT Scores',
-    'gold-icons theme-verylightgray',
+    'gold-icons',
     ''
 ); ?>
 
