42c97c70385dba7d626dfdee43a4fd272fa3d0f4
Jeff Macaluso <samacaluso@vassar.edu>
2022-10-12 09:37:33 -0400

    In the media
    new items

--- alumni/community/alumni-news/index.php
+++ alumni/community/alumni-news/index.php
@@ -107,6 +107,26 @@ $page_info = json_decode($page_info, true);
   <?php echo item_col(); ?>
 
   <div class="other-news-item plain-card-1">
+        <p><strong>David Ambroz ’02</strong> spoke to <a href="https://www.npr.org/2022/09/13/1122621426/a-place-called-home-is-a-vivid-story-of-author-david-ambrozs-childhood">NPR</a> about surviving on the streets of New York City as a homeless child and about his new memoir, <em>A Place Called Home</em>.</p>
+    </div>
+
+    <div class="other-news-item plain-card-1">
+        <p>Filmmaker <strong>Noah Baumbach ’91</strong> talked about his latest film, an adaptation of Don DeLillo’s <em>White Noise</em>, as noted in <a href="https://www.yahoo.com/entertainment/noah-baumbach-ambitious-white-noise-130007170.html">Yahoo! Entertainment</a>.</p>
+    </div>
+
+    <div class="other-news-item plain-card-1">
+        <p>The <a href="https://loyolamaroon.com/10035929/briefs/loyola-grapples-with-unexpected-passing-of-college-of-business-professor/"><em>Maroon</em></a> remembered accomplished professor <strong>Iris Mack ’78</strong>, who was the first Black woman to teach applied mathematics at the MIT Sloan School of Management.</p>
+    </div> 
+
+    <div class="other-news-item plain-card-1">
+        <p>Attorney <strong>Carrie Goldberg ’99</strong> was quoted in an <a href="https://www.npr.org/2022/10/09/1127686507/amazon-suicide-teenagers-poison">NPR</a> story about a lawsuit against Amazon for allegedly selling suicide kits to teenagers.</p>
+    </div> 
+
+    <div class="other-news-item plain-card-1">
+        <p><strong>Mariya Abdulkaf ’15</strong> was named Supervising Audience Editor of the <em>New York Times</em> Video Department, as noted by the <a href="https://www.nytco.com/press/mariya-abdulkaf-joins-the-video-department/">NYT</a>.</p>
+    </div> 
+
+  <!-- <div class="other-news-item plain-card-1">
     <p><strong>Shoshanah Bewlay ’92</strong>, the Executive Director of the New York State Committee on Open Government, talked about her work and the work of her organization on PBS’s <em><a href="https://www.pbs.org/video/shoshanah-bewlay-extended-interview-adjh5c/">Connect NY</a></em>.</p>
   </div>
 
@@ -116,12 +136,29 @@ $page_info = json_decode($page_info, true);
 
     <div class="other-news-item plain-card-1">
       <p>Computer pioneer Rear Admiral <strong>Grace Hopper</strong>, class of 1928, was the subject of an <em><a href="https://www.theengineer.co.uk/content/in-depth/late-great-engineers-grace-hopper-english-speaking">Engineer</a></em> story.</p>
-    </div>
+    </div> -->
 
   <?php echo end_item_col(); ?>
 
   <?php echo item_col(); ?>
+
     <div class="other-news-item plain-card-1">
+        <p>Author and leader of the School Garden Support Organization <strong>Whitney Cohen ’99</strong> of Life Lab will be the keynote speaker at a conference on school and youth gardening, as noted in a <a href="https://www.dispatch.com/story/lifestyle/home-garden/2022/10/08/school-garden-conference-to-take-place-oct-28-on-osu-campus/69539851007/"><em>Columbus Dispatch</em></a> article.</p>
+    </div>
+
+    <div class="other-news-item plain-card-1">
+        <p><strong>Frederick Deknatel ’08</strong>, Executive Editorof<em> Democracy in Exile</em>, wrote a chapter on reconstruction in Syria in the recently published <em>Cultural Heritage and Mass Atrocities</em>, as noted by <a href="https://www.getty.edu/publications/cultural-heritage-mass-atrocities/part-2/12-deknatel/">Getty</a>.</p>
+    </div>
+
+    <div class="other-news-item plain-card-1">
+        <p><strong>Whitney W. Donhauser ’89</strong>, former Director and President of the Museum of the City of New York, was named Deputy Director and Chief Advancement Officer at the Metropolitan Museum of Art, as noted by <a href="https://thecitylife.org/2022/10/05/the-metropolitan-museum-of-art-appoints-whitney-w-donhauser-as-deputy-director-and-chief-advancement-officer/">TheCityLife.org</a>.</p>
+    </div> 
+
+    <div class="other-news-item plain-card-1">
+        <p>The work of photographer <strong>Caleb Stein ’17</strong> was the subject of an <a href="https://www.argonautnews.com/arts_and_events/take-me-to-the-river/article_29cd5c3c-3f6f-11ed-93b0-83989b23b8d1.html"><em>Argonaut</em></a> story.</p>
+    </div> 
+
+    <!-- <div class="other-news-item plain-card-1">
       <p><strong>Maurice F. Edelson ’85</strong> was named Executive Vice President and Chief Legal Officer of WWE, as noted by <a href="https://www.marketscreener.com/quote/stock/WORLD-WRESTLING-ENTERTAIN-14926/news/WWE-R-Names-Dan-Ventrelle-EVP-Talent-Maurice-F-Edelson-EVP-Chief-Legal-Officer-41731467/">MarketScreener</a>.</p>
     </div>
 
@@ -131,7 +168,7 @@ $page_info = json_decode($page_info, true);
 
     <div class="other-news-item plain-card-1">
       <p><strong>B. David Naidu ’90</strong> was honored with the Summit Municipal Democratic Committee’s Spirit of Summit Award, as noted by <a href="https://www.tapinto.net/towns/summit/categories/press-releases/articles/smdc-s-spirit-of-summit-award-dinner-set-for-sept-18">Tap into Summit</a>.</p>
-    </div>
+    </div> -->
 
   <?php echo end_item_col(); ?>
 
