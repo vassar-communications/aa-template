81209a77eafa437ba9201e7bedb3a79f7657c618
csilverman <chris@csilverman.com>
2023-03-30 14:30:16 -0400

    Alums: added Class Note button to Alums footer (#236)

--- alums/inc/_footer-mod-optional.php
+++ alums/inc/_footer-mod-optional.php
@@ -16,6 +16,10 @@
 <div class="button-bar">
   <ul id="block-fixedalumsmyaccountdirectory">
     <li>
+      <a href="https://securelb.imodules.com/s/1654/22/events.aspx?sid=1654&gid=2&pgid=2783&cid=4689" class="btn btn-primary">Submit Class Note</a>
+    </li>
+
+    <li>
       <a href="https://connect.vassar.edu/s/1654/alumni/index.aspx?sid=1654&amp;gid=2&amp;pgid=95&amp;cid=271" class="btn btn-primary">My Account</a>
     </li>
     <li>
