036d1f5a7a3382e7aab9d52bf641d0e9bfc3b213
SusanB <subrkich@vassar.edu>
2023-09-14 15:54:20 -0400

    Additional minor change.

--- alums/inc/pages/alums-calendar-events.php
+++ alums/inc/pages/alums-calendar-events.php
@@ -3,7 +3,7 @@
         <div class="card-body">
             <h4 class="card-title card-title h2 mb-3">Calendar of Events</h4>
             <div class="card-text">
-                <p>Learn about and register for upcoming alums events.</p>
+                <p>Learn about and register for upcoming events.</p>
                 <i class="mx-0 mt-5 circle fa-solid fa-arrow-right"></i>
             </div>
         </div>
