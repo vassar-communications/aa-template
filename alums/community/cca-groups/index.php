0035b3d2211ef7a5030f669b56a9549ab0e7e702
mogange <mogange@vassar.edu>
2023-11-01 16:41:43 -0400

    Fix script url and hide tab content by default.

--- alums/community/cca-groups/index.php
+++ alums/community/cca-groups/index.php
@@ -38,7 +38,7 @@ $page_info = json_decode($page_info, true);
 ); ?>
 
 <!-- Start Dynamic Content -->
-<div id="cca-classes-description">
+<div id="cca-classes-description" style="display:none">
 <p>Use the decade selector or the search field to limit the display to specific classes or volunteers. Other options:</p>
 <ul>
 <li><a href="/alums/community/deceased/">View a list of deceased classmates</a></li>
@@ -47,19 +47,19 @@ $page_info = json_decode($page_info, true);
 </ul>
 <p><small><i><a href="https://www.vassar.edu/alums/contact">Not getting class communications?</a></i></small></p>
 </div>
-<div id="cca-clubs-description">
+<div id="cca-clubs-description" style="display:none">
 <p>Club “membership” is free and based on mailing address. Alums living in an area with an active club (displayed in the list below) should receive communications from the club. Current students or family members of a student may periodically receive relevant announcements from the club. <small><i><a href="https://www.vassar.edu/alums/contact">Not getting club communications?</a></i></small></p>
 <p>If you do not live within the geographical area of an active club but want to receive communications from a specific club, please reach out to us at <a href="mailto:alumengagement@vassar.edu">alumengagement@vassar.edu</a> and we will get you connected to the club(s) of your choosing.</p>
 <p>Use the area selector or the search field to limit the display to specific clubs or volunteers. Find contact information in the <a href="https://connect.vassar.edu/s/1654/22/interior.aspx?sid=1654&gid=2&pgid=6&cid=41#/Search/Simple">online alum directory</a> for individual club volunteers (login required).</p>
 </div>
-<div id="cca-affiliates-description">
+<div id="cca-affiliates-description" style="display:none">
 <p>You can find contact information for individual multicultural and affinity group officers by searching for them in the <a href="https://connect.vassar.edu/s/1654/22/interior.aspx?sid=1654&gid=2&pgid=6&cid=41#/Search/Simple">online alum directory</a> (login required).</p>
 <p><small><i><a href="https://www.vassar.edu/alums/contact">Not getting multicultural or affinity group communications?</a></i></small></p>
 </div>
 
-<link href="../dist/alums-data-min.css" rel="stylesheet">
+<link href="https://s.vassar.edu/alums_data/dist/alums-data-min.css" rel="stylesheet">
 <div id="cca-layout-dummy"></div>
-<script src="../dist/alums-data-min.js"></script>
+<script src="https://s.vassar.edu/alums_data/dist/alums-data-min.js"></script>
 <!-- End Dynamic Content -->
 
 <?php echo end_sec_regularContent(); ?>
