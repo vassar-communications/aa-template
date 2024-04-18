2a7f24f4c550d54b6ecac5168f7b0d4cff1c8e35
csilverman <chris@csilverman.com>
2024-04-18 15:34:32 -0400

    test

--- alums/community/aavc/board-members/index.php
+++ alums/community/aavc/board-members/index.php
@@ -145,7 +145,7 @@ ul.inline-list-links li a {
 <div>
 
 
-<div id="alisa-swire" style="clear: both; padding-bottom: 2rem; overflow: hidden">
+<div id="alisa-swire" style="clear: both; padding-bottom: 2rem; overflow: hidden"> 
  
  
   <h3 class="mb-4">AAVC Nominating and Governance Committee Chair</h3>
